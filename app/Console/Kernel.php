8b6f53d364ef51ceeee884c5476669dd740190ac
Krisan Alfa Timur <krisan47@gmail.com>
2015-04-30 12:29:59 +0700

    [INIT] Initial commit.

---
+++ app/Console/Kernel.php
@@ -0,0 +1,27 @@
+<?php namespace App\Console;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Container\Container;
+use Lustrar\L\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+    }
+}
